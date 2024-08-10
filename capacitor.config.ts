9f463798ddc516b4ed030580bc32e9a9fec81b38
amar-nath1 <amarat279955>
2024-08-10 15:13:42 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gapshap',
+  webDir: 'www'
+};
+
+export default config;
